d6b217d82b7fd68cc21ab3339b28f31944e4a1ce
Mohammed-Ullah <oshemullah@outlook.com>
2025-06-08 14:45:40 -0400

    Updated Test Suite

--- tests/e2e/Basketball-Reference/tests/Basketball Reference/MainDashboard.spec.js
+++ tests/e2e/Basketball-Reference/tests/Basketball Reference/MainDashboard.spec.js
@@ -14,3 +14,9 @@ test('Player Search', async({ page }) => {
   await expect(page).toHaveURL(/\/iveyja01/);
   console.log('Succesfully Navigated to Player page!');
 });
+
+test('Basketball Reference Main Page', async( {page} ) => {
+  await page.goto('https://www.sports-reference.com/');
+  await page.getByRole('link', { name: 'Basketball-Reference'}).nth(0).click();
+  await expect(page.getByRole('img', { name: 'Basketball-Reference.com Logo' })).toBeVisible();
+})
