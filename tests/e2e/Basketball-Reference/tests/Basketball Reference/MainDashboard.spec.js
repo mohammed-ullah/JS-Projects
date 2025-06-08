473c476ebcb0f2241b2b74e53eb6f454804485e8
Mohammed-Ullah <oshemullah@outlook.com>
2025-06-08 14:38:19 -0400

    Updated Test Suite

--- tests/e2e/Basketball-Reference/tests/Basketball Reference/MainDashboard.spec.js
+++ tests/e2e/Basketball-Reference/tests/Basketball Reference/MainDashboard.spec.js
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Page Load', async ({ page }) => {
+test('Page Load', async({ page }) => {
   await page.goto('https://www.basketball-reference.com//');
   await expect(page.getByRole('img', { name: 'Basketball-Reference.com Logo' })).toBeVisible();
   console.log('Main Basketball Reference page loaded!');
+});
 
+test('Player Search', async({ page }) => {
+  await page.goto('https://www.basketball-reference.com//');
   await page.getByRole('searchbox', { name: 'Enter a player, team or section name'}).fill('Jaden Ivey');
 
   await page.getByText('Jaden Ivey').nth(0).click();
   await expect(page).toHaveURL(/\/iveyja01/);
-  console.log('Succesfully Navigated to Player page!')
+  console.log('Succesfully Navigated to Player page!');
 });
