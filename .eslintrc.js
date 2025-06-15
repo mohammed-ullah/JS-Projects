5eb60d41ee4f2676fae07a5f3ceae2b6d14a657a
Mohammed-Ullah <oshemullah@outlook.com>
2025-06-15 19:58:31 -0400

    Update ESLint to correct typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,31 @@
-Rimodule.exports = {
-    env: {
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "linebreak-style": "off", // Allow Windows line endings
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.js", "**/*.test.js"],
+      env: {
+        jest: true,
         browser: true,
-        commonjs: true,
-        es2021: true,
-        node: true,
-        jest: true
+      },
+      parserOptions: {
+        sourceType: "module", // Allow ES6 imports in test files
+      },
     },
-    extends: [
-        'eslint:recommended'
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-    rules: {
-        'linebreak-style': 'off',  // Allow Windows line endings
-        'no-unused-vars': 'warn',
-        'no-console': 'off'
-    },
-    overrides: [
-        {
-            files: ['**/*.spec.js', '**/*.test.js'],
-            env: {
-                jest: true,
-                browser: true
-            },
-            parserOptions: {
-                sourceType: 'module'  // Allow ES6 imports in test files
-            }
-        }
-    ]
+  ],
 };
