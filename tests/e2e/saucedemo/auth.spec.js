d9ed9df5cefb3e7d1f7c7293608f4f32d1501dad
mohammed-ullah <oshemullah@outlook.com>
2025-07-17 19:46:36 -0400

    Fixed ES errors

--- tests/e2e/saucedemo/auth.spec.js
+++ tests/e2e/saucedemo/auth.spec.js
@@ -2,38 +2,38 @@ const { test, expect } = require("@playwright/test");
 const LoginPage = require("../../../pages/saucedemo/LoginPage.js");
 
 test.describe("Sauce Demo - Login Tests", () => {
-  let LoginPage;
+  let loginPageInstance;
 
   test.beforeEach(async ({ page }) => {
-    const LoginPage = new LoginPage(page);
-    await loginPage.navigate();
+    loginPageInstance = new LoginPage(page);
+    await loginPageInstance.navigate();
   });
 
   test("Sauce Demo - Successful Demo", async ({ page }) => {
-    await LoginPage.loginAsStandardUser();
-    await LoginPage.expectLoginSuccess();
+    await loginPageInstance.loginAsStandardUser();
+    await loginPageInstance.expectLoginSuccess();
     console.log("Successfully logged in!");
   });
 
   test("Sauce Demo - Incorrect Password - Unsuccessful Login", async ({
     page,
   }) => {
-    await LoginPage.login("standard_user", "incorrectPassword");
-    await LoginPage.expectLoginError();
+    await loginPageInstance.login("standard_user", "incorrectPassword");
+    await loginPageInstance.expectLoginError();
     console.log("Verified incorrect password doesn't log user in");
   });
 
   test("Sauce Demo - Incorrect Username - Unsuccessful Login", async ({
     page,
   }) => {
-    await LoginPage.login("incorrect_user", "secret_sauce");
-    await LoginPage.expectLoginError();
+    await loginPageInstance.login("incorrect_user", "secret_sauce");
+    await loginPageInstance.expectLoginError();
     console.log("Verified incorrect username doesn't log user in");
   });
 
   test("Sauce Demo - Empty Fields", async ({ page }) => {
-    await LoginPage.login("", "");
-    await LoginPage.expectLoginError();
+    await loginPageInstance.login("", "");
+    await loginPageInstance.expectLoginError();
     console.log("Verified error message for empty fields");
   });
 });
